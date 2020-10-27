82514c035d9f7da4689cf3850ccbf2fb39bfc936
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2020-10-26 18:30:36 -0600

    LogIn and Sign Up Basic Functionality Done. Working on Bottom navigation bar

--- app/src/main/java/com/example/bookshelf/CreateAccount.java
+++ app/src/main/java/com/example/bookshelf/CreateAccount.java
@@ -3,7 +3,9 @@ package com.example.bookshelf;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,33 +13,44 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccount extends AppCompatActivity {
 
-    private EditText userFullName;
-    private EditText userName;
-    private EditText user_email;
-    private EditText userPhone;
-    private EditText userPassword;
+    private EditText userFullNameEt;
+    private EditText userNameEt;
+    private EditText user_emailEt;
+    private EditText userPhoneEt;
+    private EditText userPasswordEt;
     private Button createAccountBtn;
     private TextView signInText;
 
     //Firebase Authentication
     private FirebaseAuth userAuth;
 
+    //Firebase Cloud Firestore
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        userFullName = findViewById(R.id.full_name);
-        userName = findViewById(R.id.user_name);
-        user_email = findViewById(R.id.user_email);
-        userPhone = findViewById(R.id.phone_number);
-        userPassword = findViewById(R.id.user_pwd);
+        userFullNameEt = findViewById(R.id.create_account_full_name);
+        userNameEt = findViewById(R.id.create_account_user_name);
+        user_emailEt = findViewById(R.id.create_account_user_email);
+        userPhoneEt = findViewById(R.id.create_account_phone_number);
+        userPasswordEt = findViewById(R.id.create_account_user_pwd);
         signInText = findViewById(R.id.sign_in_text);
         createAccountBtn = findViewById(R.id.create_account_btn);
 
@@ -51,18 +64,44 @@ public class CreateAccount extends AppCompatActivity {
                 //TODO: Add password verification and password masking
                 //TODO: Add phoneNumber verification
                 //TODO: Add userName Verification
-                userAuth.createUserWithEmailAndPassword(user_email.getText().toString(), userPassword.getText().toString())
+                //TODO: Add Is empty checks
+                userAuth.createUserWithEmailAndPassword(user_emailEt.getText().toString(), userPasswordEt.getText().toString())
                         .addOnCompleteListener(CreateAccount.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
-                                    Toast.makeText(getApplicationContext(), "User Sucessfully created. Check Console", Toast.LENGTH_SHORT ).show();
+                                    FirebaseUser authenticatedUser = userAuth.getCurrentUser();
+
+                                    //Add user data to cloud database
+                                    Map<String, String> user = new HashMap<>();
+                                    user.put("fullname", userFullNameEt.getText().toString());
+                                    user.put("username", userNameEt.getText().toString());
+                                    user.put("email", user_emailEt.getText().toString());
+                                    user.put("phone", userPhoneEt.getText().toString());
+
+                                    //Add user to database using userID in authentication
+                                    db.collection("users").document(authenticatedUser.getUid()).set(user);
+
                                     //Start Profile Page activity
+                                    //Pass authenticated user id into profile page
+                                    Intent profilePageIntent = new Intent(getApplicationContext(), UserProfile.class);
+                                    profilePageIntent.putExtra("UserID", authenticatedUser.getUid());
+
+                                    startActivity(profilePageIntent);
                                 }else{
                                     Toast.makeText(getApplicationContext(), "User FAILED TO create. Check Console", Toast.LENGTH_SHORT ).show();
                                 }
                             }
                         });
+            }
+        });
+
+        //Switching to signin page on click
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = new Intent(getApplicationContext(), Signin.class);
+                startActivity(signInIntent);
             }
         });
 
--- app/src/main/java/com/example/bookshelf/Signin.java
+++ app/src/main/java/com/example/bookshelf/Signin.java
@@ -1,15 +1,81 @@
 package com.example.bookshelf;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signin extends AppCompatActivity {
 
+    private EditText userEmailEt;
+    private EditText userPassEt;
+    private Button logInBtn;
+    private TextView createAccountText;
+
+    private FirebaseAuth userAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = userAuth.getCurrentUser();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        userEmailEt = findViewById(R.id.signin_user_email);
+        userPassEt = findViewById(R.id.signin_password);
+        logInBtn = findViewById(R.id.signin_button);
+        createAccountText = findViewById(R.id.create_account_text);
+
+        //Verifying user with firebase auth.
+        userAuth = FirebaseAuth.getInstance();
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userAuth.signInWithEmailAndPassword(userEmailEt.getText().toString(), userPassEt.getText().toString())
+                        .addOnCompleteListener(Signin.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser authenticatedUser = userAuth.getCurrentUser();
+                                    //Pass authenticated user id into profile page
+                                    Intent profilePageIntent = new Intent(getApplicationContext(), UserProfile.class);
+                                    profilePageIntent.putExtra("UserID", authenticatedUser.getUid());
+                                    startActivity(profilePageIntent);
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Sign In failed. Please verify email and password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        //Starting Create Account Activity
+        createAccountText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createAccontIntent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivity(createAccontIntent);
+            }
+        });
+
+
 
 
     }
