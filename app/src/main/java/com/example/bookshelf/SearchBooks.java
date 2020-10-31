fdb882fb4aa608e801afed02711316a74c642970
Petar Cvijovic <cvijovic@ualberta.ca>
2020-10-30 19:08:29 -0600

    basic search function implemented with regex

--- app/src/main/java/com/example/bookshelf/SearchBooks.java
+++ app/src/main/java/com/example/bookshelf/SearchBooks.java
@@ -6,12 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class SearchBooks extends AppCompatActivity {
     TextView uidTv;
+    ListView searchResults;
+    EditText searchBar;
+    Button searchButton;
+    ArrayAdapter<String> bookAdapter;
+    ArrayList<String> resultList; // using string dummy values until book objects can be used
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +33,45 @@ public class SearchBooks extends AppCompatActivity {
         setContentView(R.layout.activity_search_books);
 
         final String userId = getIntent().getStringExtra("UserID");
-        uidTv = findViewById(R.id.uid_search);
-        uidTv.setText(userId);
+//        uidTv = findViewById(R.id.uid_search);
+//        uidTv.setText(userId);
+
+        searchBar = findViewById(R.id.search_bar);
+        searchButton = findViewById(R.id.search_button);
+        searchResults = findViewById(R.id.search_results);
+
+        resultList = new ArrayList<>();
+        bookAdapter = new ArrayAdapter<>(this, R.layout.content, resultList);
+        searchResults.setAdapter(bookAdapter);
+
+        final String []books ={"A Song of Ice and Fire", "Swan Song", "Frankenstein", "Monster", "Monsters Inc."};
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                bookAdapter.clear();
+                String check = searchBar.getText().toString();
+                Pattern pattern = Pattern.compile(check, Pattern.CASE_INSENSITIVE);
+                for(int i = 0; i < books.length; i++){
+                    Matcher matcher = pattern.matcher(books[i]);
+                    if(matcher.find()){
+                        bookAdapter.add(books[i]);
+                    }
+                }
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
 
         //BOTTOM NAVIGATION_________________________________________________________________________
         //Initialize nav bar and assign it
