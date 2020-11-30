d17e52710f828955c1a8fc824237bb7524634804
hgshah <hgshah@ualberta.ca>
2020-11-30 12:02:57 -0700

    testing

---
+++ app/src/androidTest/java/com/example/bookshelf/BookActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.bookshelf;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onOkPressed() {
+    }
+
+    @Test
+    public void openUserProfile() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+
+    @Test
+    public void testOnOkPressed() {
+    }
+}
