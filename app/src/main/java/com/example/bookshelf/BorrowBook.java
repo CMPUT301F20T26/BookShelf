a9aacc0c29c3f7b9b7341d53284c73e72461af54
patelyogini3 <43617607+patelyogini3@users.noreply.github.com>
2020-11-29 21:45:26 -0700

    Book added to test borrow

--- app/src/main/java/com/example/bookshelf/BorrowBook.java
+++ app/src/main/java/com/example/bookshelf/BorrowBook.java
@@ -2,6 +2,8 @@ package com.example.bookshelf;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +17,27 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class BorrowBook extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
     LatLng location = new LatLng(53.528333, -113.528917); // LatLng type used by the maps API to specify a location
     boolean needsInit = false;
+    private ListView bookList;
+    private ArrayAdapter<Book> bookAdapter;
+    private ArrayList<Book> bookDataList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.borrow_book);
+        bookDataList = new ArrayList<>();
+        bookAdapter = new BookArrayAdapter(this,bookDataList);
+        bookList.setAdapter(bookAdapter);
+        Long isbn = Long.valueOf(123452);
+        Book test_Book = new Book("Test","Test",isbn,"test", Book.BookStatus.Available,"Me","blah");
+        bookDataList.add(test_Book);
+        bookAdapter.notifyDataSetChanged();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
