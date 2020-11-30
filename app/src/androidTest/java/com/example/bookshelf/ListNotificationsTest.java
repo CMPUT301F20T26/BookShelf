46cb3715658c4ea837bd0b47ef839b16c65bfa08
hgshah <hgshah@ualberta.ca>
2020-11-30 13:39:00 -0700

    testing functions

---
+++ app/src/androidTest/java/com/example/bookshelf/ListNotificationsTest.java
@@ -0,0 +1,102 @@
+package com.example.bookshelf;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListNotificationsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void setStatus() {
+    }
+
+    @Test
+    public void setNotificationID() {
+    }
+
+    @Test
+    public void setDate() {
+    }
+
+    @Test
+    public void setMeetUpLocation() {
+    }
+
+    @Test
+    public void setOwner() {
+    }
+
+    @Test
+    public void setRequester() {
+    }
+
+    @Test
+    public void setBook() {
+    }
+
+    @Test
+    public void setOwnerName() {
+    }
+
+    @Test
+    public void setRequesterName() {
+    }
+
+    @Test
+    public void setBookName() {
+    }
+
+    @Test
+    public void getStatus() {
+    }
+
+    @Test
+    public void getNotificationID() {
+    }
+
+    @Test
+    public void getDate() {
+    }
+
+    @Test
+    public void getMeetUpLocation() {
+    }
+
+    @Test
+    public void getOwnerID() {
+    }
+
+    @Test
+    public void getRequesterID() {
+    }
+
+    @Test
+    public void getBookID() {
+    }
+
+    @Test
+    public void getOwnerName() {
+    }
+
+    @Test
+    public void getRequesterName() {
+    }
+
+    @Test
+    public void getBookName() {
+    }
+}
